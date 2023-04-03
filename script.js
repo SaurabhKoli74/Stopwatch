c3c9e6db1357ef086fddafdf0c4ef71ac417f15b
Saurabh <saurabh.koli7420@gmail.com>
2023-04-03 13:17:01 +0530

    Inital Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const start = document.getElementById('start');
+const stop = document.getElementById('stop');
+const reset = document.getElementById('reset');
+
+
+
+function startTimer(){
+    console.log('start timer')
+}
+start.addEventListener('click',startTimer);

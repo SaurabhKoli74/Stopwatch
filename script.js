5388d40d2cb851da9c18908e7c1869c0c7700352
Saurabh <saurabh.koli7420@gmail.com>
2023-04-03 15:12:22 +0530

    Add reset functionality and design the layout

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
-const start = document.getElementById('start');
-const stop = document.getElementById('stop');
-const reset = document.getElementById('reset');
+const startbtn = document.getElementById('start');
+const stopbtn = document.getElementById('stop');
+const resetbtn = document.getElementById('reset');
 const min = document.getElementById('min');
 const sec = document.getElementById('sec');
 var isStart = false;
 var isStop = false;
-var timer=null;
+var timer = null;
 
-function stopTimer(){
-    if(!isStop){
+function stopTimer() {
+    if (!isStop) {
         clearInterval(timer)
-        isStop=true;
-        isStart=false;
+        isStop = true;
+        isStart = false;
     }
 }
 
+function resetTimer() {
+
+    if (isStart) {
+        // console.log('inside resetTimer')
+        clearInterval(timer);
+        min.innerText = '00';
+        sec.innerText = '00';
+        isStart = false;
+
+
+    } else if (isStop) {
+        min.innerText = '00';
+        sec.innerText = '00';
+        isStop = false;
+    }
+}
 function startTimerInterval() {
 
-    
+
 
     let a = parseInt(sec.innerText);
     if (a + 1 === 60) {
-        sec.innerText='00';
+        sec.innerText = '00';
         let b = parseInt(min.innerText);
-       
+
         if (b <= 8)
             min.innerText = '0' + (b + 1);
         else {
@@ -39,22 +55,25 @@ function startTimerInterval() {
     }
 
 
-    
+
 }
 
 function startTimer() {
     // console.log(min.innerText)
     // console.log(min.innertext)
     if (isStart) {
-        return ;
-    }   
-    
-    isStart=true;
-    isStop=false;
-    timer=setInterval(startTimerInterval, 1000)
-    
+        return;
+    }
 
-    // }
+    console.log(isStart)
+
+    isStart = true;
+    isStop = false;
+    timer = setInterval(startTimerInterval, 1000)
+
+
+
 }
-start.addEventListener('click', startTimer);
-stop.addEventListener('click',stopTimer)
+startbtn.addEventListener('click', startTimer);
+stopbtn.addEventListener('click', stopTimer);
+resetbtn.addEventListener('click', resetTimer)

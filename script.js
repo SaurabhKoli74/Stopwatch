3b53240f5df1ddbf24fc25956071d6260a62bb24
Saurabh <saurabh.koli7420@gmail.com>
2023-04-03 14:21:35 +0530

    Implement Start and Stop Functionality of Timer

--- script.js
+++ script.js
@@ -1,10 +1,60 @@
 const start = document.getElementById('start');
 const stop = document.getElementById('stop');
 const reset = document.getElementById('reset');
+const min = document.getElementById('min');
+const sec = document.getElementById('sec');
+var isStart = false;
+var isStop = false;
+var timer=null;
 
-
-
-function startTimer(){
-    console.log('start timer')
+function stopTimer(){
+    if(!isStop){
+        clearInterval(timer)
+        isStop=true;
+        isStart=false;
+    }
 }
-start.addEventListener('click',startTimer);
+
+function startTimerInterval() {
+
+    
+
+    let a = parseInt(sec.innerText);
+    if (a + 1 === 60) {
+        sec.innerText='00';
+        let b = parseInt(min.innerText);
+       
+        if (b <= 8)
+            min.innerText = '0' + (b + 1);
+        else {
+            min.innerText = b + 1;
+        }
+
+
+    }
+    else if (a <= 8)
+        sec.innerText = '0' + (a + 1);
+    else {
+        sec.innerText = a + 1;
+    }
+
+
+    
+}
+
+function startTimer() {
+    // console.log(min.innerText)
+    // console.log(min.innertext)
+    if (isStart) {
+        return ;
+    }   
+    
+    isStart=true;
+    isStop=false;
+    timer=setInterval(startTimerInterval, 1000)
+    
+
+    // }
+}
+start.addEventListener('click', startTimer);
+stop.addEventListener('click',stopTimer)
